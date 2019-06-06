e4332c30ab34f5682237d117ceccad78d1e0748b
Jesse Stone <jstone@psi.edu>
2019-06-06 16:41:15 -0700

    added html generation.

--- IngestLddView/markdown.sh
+++ IngestLddView/markdown.sh
@@ -13,3 +13,4 @@ LDD_FILE=$1
 saxon -xsl:"${SCRIPT_DIR}"/IngestLddMarkdown.xsl -s:"${LDD_FILE}" -o:"${LDD_FILE}".md dictfile="${LDD_FILE}"
 
 pandoc --css pandoc.css --pdf-engine=${PDF_ENGINE} -o "${LDD_FILE}".pdf ${LDD_FILE}.md
+pandoc --css pandoc.css -o "${LDD_FILE}".html ${LDD_FILE}.md
