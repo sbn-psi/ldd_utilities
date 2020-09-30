38001862f583cb41f9ba0a3c3f8fcf68b4cb2cac
Jesse Stone <jstone@psi.edu>
2020-09-30 09:25:01 -0700

    better accounts for basenames

--- IngestLddView/uml2pdf.sh
+++ IngestLddView/uml2pdf.sh
@@ -1,1 +1,6 @@
-plantuml -tsvg geom-split.xml.plantuml &&  inkscape -o geom-split.xml.pdf --export-type=pdf geom-split.xml.svg && open geom-split.xml.pdf
+UMLFILE=$1
+BASENAME=$(echo "$UMLFILE" | sed -e 's/.plantuml//')
+SVGFILE="$BASENAME".svg
+PDFFILE="$BASENAME".pdf
+
+plantuml -tsvg $UMLFILE &&  inkscape -o $PDFFILE --export-type=pdf $SVGFILE
