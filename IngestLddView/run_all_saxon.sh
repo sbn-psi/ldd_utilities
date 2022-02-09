c5130c6c1561ec60d9b77ffc2dff8de34d7409ad
Jesse Stone <jstone@psi.edu>
2022-02-09 08:16:07 -0700

    added status messages, reverted extension

--- IngestLddView/run_all_saxon.sh
+++ IngestLddView/run_all_saxon.sh
@@ -4,12 +4,16 @@ set -e
 SCRIPT_DIR=`dirname $0`
 LDD_FILE=$1
 
+echo "Creating html file..."
 saxon -xsl:"${SCRIPT_DIR}"/IngestLddView.xsl -s:"${LDD_FILE}" -o:"${LDD_FILE}".html
 
+echo "Creating dot file..."
 saxon -xsl:"${SCRIPT_DIR}"/IngestLddDot.xsl -s:"${LDD_FILE}" -o:"${LDD_FILE}".dot
 
+echo "Creating pdf file..."
 dot -Tpdf -O "${LDD_FILE}".dot
 
+echo "Creating uml pdf file..."
 saxon -xsl:"${SCRIPT_DIR}"/IngestLddPlantUml.xsl -s:"${LDD_FILE}" -o:"${LDD_FILE}".uml
-plantuml -tsvg "${LDD_FILE}".plantuml 
+plantuml -tsvg "${LDD_FILE}".uml 
 open "${LDD_FILE}".svg
