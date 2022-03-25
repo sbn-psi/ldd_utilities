72df6d13c8446f766671772caf7502ee4b736bbc
Jesse Stone <jstone@psi.edu>
2022-03-25 13:49:13 -0700

    added basic mutation support

--- LddTestGenerator/lddtestgen.py
+++ LddTestGenerator/lddtestgen.py
@@ -5,8 +5,11 @@ import argparse
 import json
 import os
 import os.path
+from io import BytesIO
+from xml.etree.ElementTree import ElementTree
 
 import jinja2
+from lxml import etree
 
 
 def main():
@@ -19,23 +22,49 @@ def main():
 
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(args.template_dir))
 
-    tests = json.load(open(args.json_file))
+    config = json.load(open(args.json_file))
+    tests = config["tests"]
+    nsmap = config["namespaces"]
 
     for test in tests:
-        create_test(test["product_id"], test["test_type"], test["template_file"], test["snippet_files"], env, args.snippet_dir, args.output_dir)
+        create_test(test, env, nsmap, args.snippet_dir, args.output_dir)
 
-def create_test(product_id, test_type, template_file, snippet_files, env, snippet_dir, output_dir):
-    template = env.get_template(template_file)
-    snippets = {name: load_snippet(filename, snippet_dir) for name, filename in snippet_files.items()}
-    contents = template.render(product_id=product_id, snippets=snippets)
+def create_test(test, env, nsmap, snippet_dir, output_dir):
+    
+    template = env.get_template(test["template_file"])
+    snippets = {name: load_snippet(filename, snippet_dir) for name, filename in test["snippet_files"].items()}
+    contents = template.render(product_id=test["product_id"], snippets=snippets)
+    doc = apply_mutations(contents, test.get("mutations", []), nsmap)
+
     os.makedirs(output_dir, exist_ok=True)
-    with open(os.path.join(output_dir, f"{product_id}_{test_type}.xml"), "w") as f:
-        f.write(contents)
+    filename = os.path.join(output_dir, f"{test['product_id']}_{test['test_type']}.xml")
+    doc.write(filename, encoding="utf-8", xml_declaration=True, pretty_print=True)
 
 
 def load_snippet(filename, snippet_dir):
     with open(os.path.join(snippet_dir, filename)) as f:
         return f.read()
+
+def apply_mutations(contents, mutations, nsmap) -> ElementTree:
+    doc = etree.parse(BytesIO(contents.encode('utf-8')))
+    for mutation in mutations:
+        apply_mutation(doc, mutation, nsmap)
+    return doc
+
+def apply_mutation(doc:ElementTree, mutation, nsmap):
+    xpath = mutation['xpath']
+    operation = mutation['operation']
+
+    print (xpath)
+    for e in doc.findall(xpath, nsmap):
+        if operation == 'delete':
+            e.getparent().remove(e)
+        if operation == 'changeValue':
+            value = mutation["value"]
+            e.text = value
+        if operation == 'changeUnit':
+            value = mutation["value"]
+            e.attrib["unit"] = value
 
 if __name__ == "__main__":
     sys.exit(main())
