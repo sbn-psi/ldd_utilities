dabe56b72be274f646164eafe1c1a3e386033624
Jesse Stone <jstone@psi.edu>
2022-03-31 09:53:53 -0700

    extracted TestBuilder class

--- LddTestGenerator/lddtestgen.py
+++ LddTestGenerator/lddtestgen.py
@@ -28,74 +28,83 @@ def main():
     nsmap = config.get("namespaces", {})
     suites = config.get("suites", [])
 
+    builder = TestBuilder(env, nsmap, args.snippet_dir, args.output_dir)
+
     for suite in suites:
-        create_suite(suite, env, nsmap, args.snippet_dir, args.output_dir)
+        builder.create_suite(suite)
 
     for test in tests:
-        create_test(test, env, nsmap, args.snippet_dir, args.output_dir)
+        builder.create_test(test)
 
-def create_suite(suite, env, nsmap, snippet_dir, output_dir):
-    for test in suite["tests"]:
-        create_test(test, env, nsmap, snippet_dir, output_dir, suite=suite)
+class TestBuilder:
+    def __init__(self, env, nsmap, snippet_dir, output_dir):
+        self.env = env
+        self.nsmap = nsmap
+        self.snippet_dir = snippet_dir
+        self.output_dir = output_dir
 
-def create_test(test, env, nsmap, snippet_dir, output_dir, suite={}):
-    template_file = test.get("template_file", suite.get("template_file"))
-    template = env.get_template(template_file)
+    def create_suite(self, suite):
+        for test in suite["tests"]:
+            self.create_test(test, suite=suite)
 
-    snippet_list = test.get("snippet_files", suite.get("snippet_files")).items()
-    snippets = {name: load_snippet(filename, snippet_dir) for name, filename in snippet_list}
+    def create_test(self, test, suite={}):
+        template_file = test.get("template_file", suite.get("template_file"))
+        template = self.env.get_template(template_file)
 
-    suite_description = "\n".join(textwrap.wrap(suite.get("description", "")))
-    test_description = "\n".join(textwrap.wrap(test.get("description", "")))
-    suite_id = suite.get("id", "")
-    test_id = test.get("id", "")
+        snippet_list = test.get("snippet_files", suite.get("snippet_files")).items()
+        snippets = {name: self.load_snippet(filename) for name, filename in snippet_list}
 
-    product_id = "_".join(x for x in [suite_id, test_id] if x)
-    
-    contents = template.render(
-        product_id=product_id, 
-        snippets=snippets, 
-        test_description=test_description, 
-        suite_description=suite_description,
-        test_id=test_id,
-        suite_id=suite_id)
-    doc = apply_mutations(contents, test.get("mutations", []), nsmap)
+        suite_description = "\n".join(textwrap.wrap(suite.get("description", "")))
+        test_description = "\n".join(textwrap.wrap(test.get("description", "")))
+        suite_id = suite.get("id", "")
+        test_id = test.get("id", "")
 
-    os.makedirs(output_dir, exist_ok=True)
-    filename = os.path.join(output_dir, f"{product_id}_{test['test_type']}.xml")
-    doc.write(filename, encoding="utf-8", xml_declaration=True, pretty_print=True)
+        product_id = "_".join(x for x in [suite_id, test_id] if x)
+        
+        contents = template.render(
+            product_id=product_id, 
+            snippets=snippets, 
+            test_description=test_description, 
+            suite_description=suite_description,
+            test_id=test_id,
+            suite_id=suite_id)
+        doc = self.apply_mutations(contents, test.get("mutations", []), self.nsmap)
+
+        os.makedirs(self.output_dir, exist_ok=True)
+        filename = os.path.join(self.output_dir, f"{product_id}_{test['test_type']}.xml")
+        doc.write(filename, encoding="utf-8", xml_declaration=True, pretty_print=True)
 
 
-def load_snippet(filename, snippet_dir):
-    with open(os.path.join(snippet_dir, filename)) as f:
-        return f.read()
+    def load_snippet(self, filename):
+        with open(os.path.join(self.snippet_dir, filename)) as f:
+            return f.read()
 
-def apply_mutations(contents, mutations, nsmap) -> ElementTree:
-    doc = etree.parse(BytesIO(contents.encode('utf-8')))
-    for mutation in mutations:
-        apply_mutation(doc, mutation, nsmap)
-    return doc
+    def apply_mutations(self, contents, mutations, nsmap) -> ElementTree:
+        doc = etree.parse(BytesIO(contents.encode('utf-8')))
+        for mutation in mutations:
+            self.apply_mutation(doc, mutation, nsmap)
+        return doc
 
-def apply_mutation(doc:ElementTree, mutation, nsmap):
-    xpath = mutation['xpath']
-    operation = mutation['operation']
+    def apply_mutation(self, doc:ElementTree, mutation, nsmap):
+        xpath = mutation['xpath']
+        operation = mutation['operation']
 
-    for e in doc.findall(xpath, nsmap):
-        if operation == 'delete':
-            addcomment(e, "Deleted element")
-            e.getparent().remove(e)
-        if operation == 'changeValue':
-            value = mutation["value"]
-            addcomment(e, f"Change value from '{e.text}' to '{value}'")
-            e.text = value
-        if operation == 'changeUnit':
-            value = mutation["value"]
-            oldvalue = e.attrib["unit"]
-            addcomment(e, f"Change unit from '{oldvalue}' to '{value}'")
-            e.attrib["unit"] = value
+        for e in doc.findall(xpath, nsmap):
+            if operation == 'delete':
+                self.addcomment(e, "Deleted element")
+                e.getparent().remove(e)
+            if operation == 'changeValue':
+                value = mutation["value"]
+                self.addcomment(e, f"Change value from '{e.text}' to '{value}'")
+                e.text = value
+            if operation == 'changeUnit':
+                value = mutation["value"]
+                oldvalue = e.attrib["unit"]
+                self.addcomment(e, f"Change unit from '{oldvalue}' to '{value}'")
+                e.attrib["unit"] = value
 
-def addcomment(e, message):
-    e.getparent().append(etree.Comment(f"lddtestgen:: {e.tag}: {message}"))
+    def addcomment(self, e, message):
+        e.getparent().append(etree.Comment(f"lddtestgen:: {e.tag}: {message}"))
 
 if __name__ == "__main__":
     sys.exit(main())
