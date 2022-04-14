7b3aeabe53daa32bb8a9e2827780b24c6b6837de
Jesse Stone <jstone@psi.edu>
2022-04-14 09:55:41 -0700

    Now accepts an output file as an argument

--- LddDescriptionExtractor/extract_defs.py
+++ LddDescriptionExtractor/extract_defs.py
@@ -3,23 +3,27 @@
 import argparse
 import os
 import re
+import itertools
 from lxml import etree
+
 
 NSMAP = {"pds": "http://pds.nasa.gov/pds4/pds/v1"}
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("file_name")
+    parser.add_argument("output_file")
     args = parser.parse_args()
     doc = etree.parse(args.file_name)
 
     classes = findall(doc, "//pds:DD_Class[pds:element_flag='true']")
-    for cls in classes:
-        print_class(cls, doc)
+    lines = itertools.chain.from_iterable(print_class(cls, doc) for cls in classes)
+    with open(args.output_file, "w") as dest:
+        dest.write("\r\n".join(lines))
     
 def print_class(cls, doc):
     cls_name = find(cls, "pds:name").text
     cls_def = element_text(find(cls, "pds:definition"))
-    print(f'{cls_name},,"{cls_def}"')
+    yield(f'{cls_name},,"{cls_def}"')
 
     class_associations = findall(cls, "pds:DD_Association[pds:reference_type='component_of']")    
     for association in class_associations:
@@ -27,7 +31,8 @@ def print_class(cls, doc):
         for class_id in class_ids:
             cls2 = find(doc, f"//pds:DD_Class[pds:local_identifier='{class_id}']")
             if cls2 is not None:
-                print_class(cls2, doc)
+                for x in print_class(cls2, doc):
+                    yield(x)
             else:
                 print(f"Could not find class: {class_id}")
 
@@ -39,7 +44,7 @@ def print_class(cls, doc):
             attribute_name = find(attribute, "pds:name").text
             attribute_def = element_text(find(attribute, "pds:definition"))
         
-            print(f'{cls_name},{attribute_name},"{attribute_def}"')
+            yield(f'{cls_name},{attribute_name},"{attribute_def}"')
 
 def is_usable_reference(reference):
     return all(x not in reference for x in [".", "#"])
