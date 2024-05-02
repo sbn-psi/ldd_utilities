e86e7cc78402ff8402b31746bc80d4db158bb807
Jesse Stone <jstone@psi.edu>
2024-05-02 09:24:16 -0700

    changed pdf engine

--- IngestLddView/markdown.sh
+++ IngestLddView/markdown.sh
@@ -5,7 +5,7 @@ if [ ! -e pandoc.css ]; then
     wget https://gist.githubusercontent.com/killercup/5917178/raw/40840de5352083adb2693dc742e9f75dbb18650f/pandoc.css
 fi
 
-PDF_ENGINE=pdflatex
+PDF_ENGINE=wkhtmltopdf
 
 SCRIPT_DIR=`dirname $0`
 LDD_FILE=$1
